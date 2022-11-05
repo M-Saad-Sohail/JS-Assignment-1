275e3e9ad4b1038548f03f761d7fd334d45aea4e
M-Saad-Sohail <93836177+M-Saad-Sohail@users.noreply.github.com>
2022-11-05 13:48:03 +0500

    Add files via upload

---
+++ JS Assignment1/index.js
@@ -0,0 +1,109 @@
+// CH #1 : ALERTS
+
+// alert("Hello world")
+// alert(18)
+
+// CH #2 : VARIABLE FOR STRINGS
+
+// var name = "Saad Sohail";
+// alert(name);
+// name = "Sohail Ahmed";
+// alert(name);
+
+// CH #3 : VARIABLE FOR NUMBERS
+
+// var age = 18;
+// alert(age);
+// age = 25;
+// alert(age);
+
+// var a = 9;
+// var b = 5;
+// alert(a + b);
+
+// var a = 20;
+// alert(a);
+// a = a + 10;
+// alert(a);
+ 
+// CH #4 : VARIABLE NAMES LEGAL AND ILLEGAL
+
+// camelCase:
+
+// var pointNum = 5;
+// var slopePoint =  7 ;
+
+// variables rules
+
+// A variable name can contain only letters, numbers, dollar signs, and underscores.
+
+// CH #5 : MATH EXPRESSIONS FAMILIAR OPERATORS
+
+// SUM
+// var num = 2 + 2;
+// alert(num);
+// var newNum = num + 7;
+// alert(newNum);
+
+// SUBTRACTON
+// var num = 2 - 2;
+// alert(num);
+// var newNum = num - 7;
+// alert(newNum);
+
+// MULTIPLICATION
+// var num = 2 * 2;
+// alert(num);
+// var newNum = num * 7;
+// alert(newNum);
+
+// DIVISION
+// var num = 2 / 2;
+// alert(num);
+// var newNum = num / 7;
+// alert(newNum);
+
+// MODULAS
+// var num = 2 % 2;
+// alert(num);
+// var newNum = num % 7;
+// alert(newNum);
+
+// CH #6 : MATH EXPRESSIONS UNFAMILIAR OPERATORS
+
+// var name = 1;
+// name = name++;
+// alert(name)
+
+// var age = 1;
+// age = ++age;
+// alert(age)
+
+// var name = 1;
+// name = name--;
+// alert(name)
+
+// var age = 1;
+// age = --age;
+// alert(age)
+
+// CH #7 : MATH EXPRESSIONS ELIMINATING ABBIGUITY
+
+// var  name = (2 + 4) * 5;
+// alert(name);
+
+// var totalSalary = ((5 / 2) * 3) +1000;
+// alert(totalSalary);
+ 
+// CH #8 : CONCATENATING TEXT STRINGS
+
+// var product = 3 + 5;
+// alert(product)
+// answer is "8"
+
+// var product = "3 + 5";
+// alert(product)
+// answer is "35"
+
+
+
